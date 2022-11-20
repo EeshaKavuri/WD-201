ddc0292e4cb8c7823d1d22100feeb6d813469983
Rajendra Prasad <you@example.com>
2022-11-20 22:48:44 +0530

    First Commit

--- index.js
+++ index.js
@@ -1,51 +1,62 @@
-const http = require("http");
-const fs = require("fs");
-let args = require("minimist")(process.argv.slice(2));
-let port = args.port;
-let portstr = String(port);
+
+const http = require('http')
+const fs= require("fs");
 
 
-let homeContent = "";
-let projectContent = "";
-let registerContent = "";
 
-fs.readFile("home.html", (err, home) => {
-    if (err) {
-        throw err;
-    }
-    homeContent = home;
+
+let homecontent="";
+let filecontent="";
+let reg="";
+fs.readFile("home.html",
+(err,home) => {
+if(err){
+    throw err;
+}
+homecontent=home;
 });
 
-fs.readFile("project.html", (err, project) => {
-    if (err) {
+fs.readFile("project.html",
+(err,fil) => {
+    if(err)
+    {
         throw err;
     }
-    projectContent = project;
-});
+   filecontent=fil;
+})
 
-fs.readFile("registration.html", (err, register) => {
-    if (err) {
+
+
+
+
+
+
+
+fs.readFile("registration.html",
+(err,regf) => {
+    if(err)
+    {
         throw err;
     }
-    registerContent = register;
-});
-http
-    .createServer((request, response) => {
-        let url = request.url;
-        response.writeHeader(200, { "Content-Type": "text/html" });
-        switch (url) {
-            case "/project":
-                response.write(projectContent);
-                response.end();
-                break;
-            case "/registration":
-                response.write(registerContent);
-                response.end();
-                break;
-            default:
-                response.write(homeContent);
-                response.end();
-                break;
-        }
-    })
-    .listen(portstr);
+ reg=regf;
+})
+let args=require("minimist")(process.argv.slice(2));
+http.createServer((request,response) =>{
+    let url =request.url
+    response.writeHeader(200,{"Content-type":"text/html"})
+    switch (url)
+    {
+        case "/project":
+            response.write(filecontent)
+            response.end();
+            break
+        case "/registration":
+            response.write(reg)
+            response.end();
+            break;
+        default:
+            response.write(homecontent)
+            response.end();
+            break;
+    }
+}).listen(args["port"]);
